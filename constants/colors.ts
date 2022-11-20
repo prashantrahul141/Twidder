6c0f9901eca871b12b5980e038c040c093355d7a
Prashant <prashantrahul141@protonmail.com>
2022-11-20 17:26:40 +0530

    adding colors, global fonts, post type

--- constants/colors.ts
+++ constants/colors.ts
@@ -1,7 +1,12 @@
 let Colors = {
-  background: '#2a3048',
+  background: '#1f2024',
+  blue_background: '#2a3048',
   background_light: '#2a3048',
   standard_white: '#f3efff',
+  standard_light_white: '#c8c8c8d9',
   accent_blue: '#1976d2',
+  like_hover: 'rgba(255, 59, 59, 0.8)',
+  comment_hover: 'rgba(80, 80, 220, 0.9)',
+  share_hover: 'rgba(59, 255, 59, 0.8)',
 };
 export { Colors };
---
+++ types/types.ts
@@ -0,0 +1,11 @@
+type TypePost = {
+  authorName: string;
+  authorUsername: string;
+  authorProfile: string;
+  postTime: string;
+  likes: number;
+  textData: string;
+  photoData: null | string;
+};
+
+export type { TypePost };
