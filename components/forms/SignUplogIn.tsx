a319337677dd29d20aa9cdfacc9468701ab4b538
Prashant <prashantrahul141@protonmail.com>
2022-11-22 18:44:41 +0530

    basic signup page

---
+++ components/forms/SignUplogIn.tsx
@@ -0,0 +1,131 @@
+import VisibilityOffOutlinedIcon from '@mui/icons-material/VisibilityOffOutlined';
+import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import {
+  Box,
+  Button,
+  Divider,
+  FormControl,
+  IconButton,
+  styled,
+  TextField,
+} from '@mui/material';
+import { useState } from 'react';
+import { Colors } from '@constants/colors';
+
+const CssTextField = styled(TextField)({
+  '& input': {
+    color: Colors.standard_white,
+    fontFamily: 'Lato',
+  },
+  '& label': {
+    color: Colors.standard_light_white,
+  },
+  '& label.Mui-focused': {
+    color: Colors.standard_white,
+  },
+  '&': {
+    color: Colors.standard_white,
+  },
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: Colors.standard_light_white,
+    },
+    '&:hover fieldset': {
+      borderColor: Colors.standard_white,
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: Colors.standard_white,
+    },
+  },
+});
+
+const SignUplogIn = ({ btnString }: { btnString: string }) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
+    <FormControl
+      sx={{
+        border: `1pt solid ${Colors.standard_light_white_400}`,
+        borderRadius: '5px',
+        width: '95vw',
+        maxWidth: '400px',
+        height: 'fit-content',
+        marginTop: '50px',
+        padding: '30px',
+      }}>
+      <Box
+        sx={{
+          display: 'flex',
+          flexWrap: 'nowrap',
+          marginBottom: '20px',
+        }}>
+        <IconButton
+          sx={{
+            cursor: 'default',
+            width: '5%',
+            marginRight: '15px',
+          }}>
+          <AccountCircleOutlinedIcon
+            sx={{ color: Colors.standard_light_white }}
+          />
+        </IconButton>
+        <CssTextField
+          id='outlined-basic'
+          label='Email'
+          variant='outlined'
+          sx={{
+            width: '90%',
+          }}
+        />
+      </Box>
+
+      <Box
+        sx={{
+          marginBottom: '20px',
+          display: 'flex',
+          flexWrap: 'nowrap',
+        }}>
+        <CssTextField
+          id='outlined-basic'
+          label='Password'
+          variant='outlined'
+          type={showPassword ? 'password' : 'type'}
+          sx={{
+            width: '90%',
+          }}
+        />
+        <IconButton
+          onClick={() => setShowPassword(!showPassword)}
+          sx={{ width: '5%', marginLeft: '15px' }}>
+          {showPassword && (
+            <VisibilityOutlinedIcon
+              sx={{ color: Colors.standard_light_white }}
+            />
+          )}
+          {!showPassword && (
+            <VisibilityOffOutlinedIcon
+              sx={{ color: Colors.standard_light_white }}
+            />
+          )}
+        </IconButton>
+      </Box>
+      <Button
+        variant='outlined'
+        href=''
+        sx={{
+          height: '45px',
+          marginTop: '30px',
+        }}>
+        {btnString}
+      </Button>
+      <Divider
+        sx={{
+          marginTop: '30px',
+          borderColor: Colors.standard_light_white_400,
+        }}></Divider>
+    </FormControl>
+  );
+};
+
+export default SignUplogIn;
---
+++ pages/signup.tsx
@@ -0,0 +1,43 @@
+import CompHead from '@components/common/CompHead';
+import SignUplogIn from '@components/forms/SignUplogIn';
+import { Colors } from '@constants/colors';
+import { Box, Typography } from '@mui/material';
+import { Component } from 'react';
+
+const SignUp = () => {
+  return (
+    <>
+      <CompHead headTitle='Sign Up'></CompHead>
+      <Box
+        sx={{ display: 'block', alignContent: 'center', textAlign: 'center' }}>
+        <Box
+          sx={{
+            marginTop: '80px',
+          }}>
+          <Typography
+            variant='h6'
+            color={Colors.standard_light_white}
+            fontFamily='Lato'
+            fontSize={'0.8rem'}
+            letterSpacing={0}
+            sx={{
+              display: 'inline',
+            }}>
+            WELCOME TO&nbsp;
+            <Typography
+              color={Colors.accent_blue}
+              variant='h2'
+              letterSpacing={1.9}
+              fontFamily={'Oswald'}
+              sx={{ display: 'inline' }}>
+              TWIDDER.
+            </Typography>
+          </Typography>
+        </Box>
+        <SignUplogIn btnString='Sign up'></SignUplogIn>
+      </Box>
+    </>
+  );
+};
+
+export default SignUp;
