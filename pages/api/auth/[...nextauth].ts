c4d222f9f28474efe3c288302613b1a711377739
Prashant <prashantrahul141@protonmail.com>
2022-11-23 15:54:08 +0530

    next-auth api

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,28 @@
+import NextAuth from 'next-auth';
+import prisma from '@lib/prismaDB';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+import DiscordProvider from 'next-auth/providers/discord';
+import TwitterProvider from 'next-auth/providers/twitter';
+
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID || '',
+      clientSecret: process.env.GITHUB_CLIENT_SECRET || '',
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+    }),
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID || '',
+      clientSecret: process.env.DISCORD_CLIENT_SECRET || '',
+    }),
+  ],
+  secret: process.env.JWT_SECRET,
+};
+
+export default NextAuth(authOptions);
