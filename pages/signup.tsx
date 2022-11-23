991d0f35747860eba485dde692ef5c9d0d2a9301
Prashant <prashantrahul141@protonmail.com>
2022-11-23 15:53:38 +0530

    minor

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -2,6 +2,7 @@ import CompHead from '@components/common/CompHead';
 import SignUplogIn from '@components/forms/SignUplogIn';
 import { Colors } from '@constants/colors';
 import { Box, Typography } from '@mui/material';
+import Link from 'next/link';
 
 const SignUp = () => {
   return (
@@ -9,32 +10,34 @@ const SignUp = () => {
       <CompHead headTitle='Sign Up'></CompHead>
       <Box
         sx={{ display: 'block', alignContent: 'center', textAlign: 'center' }}>
-        <Box
-          sx={{
-            marginTop: '80px',
-          }}>
-          <Typography
-            variant='h6'
-            color={Colors.standard_light_white}
-            fontFamily='Lato'
-            fontSize={'0.8rem'}
-            letterSpacing={0}
+        <Link href='./' style={{ textDecoration: 'none' }}>
+          <Box
             sx={{
-              display: 'inline',
+              marginTop: '80px',
             }}>
-            WELCOME TO&nbsp;
-            <div>
-              <Typography
-                color={Colors.accent_blue}
-                variant='h2'
-                letterSpacing={1.9}
-                fontFamily={'Oswald'}
-                sx={{ display: 'inline' }}>
-                TWIDDER.
-              </Typography>
-            </div>
-          </Typography>
-        </Box>
+            <Typography
+              variant='h6'
+              color={Colors.standard_light_white}
+              fontFamily='Lato'
+              fontSize={'0.8rem'}
+              letterSpacing={0}
+              sx={{
+                display: 'inline',
+              }}>
+              WELCOME TO&nbsp;
+              <div>
+                <Typography
+                  color={Colors.accent_blue}
+                  variant='h2'
+                  letterSpacing={1.9}
+                  fontFamily={'Oswald'}
+                  sx={{ display: 'inline' }}>
+                  TWIDDER.
+                </Typography>
+              </div>
+            </Typography>
+          </Box>
+        </Link>
         <SignUplogIn btnString='Sign up'></SignUplogIn>
       </Box>
     </>
