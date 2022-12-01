7fdbaf06d2fd43a8580947748ee4e837070d386c
Prashant <prashantrahul141@protonmail.com>
2022-12-01 22:11:47 +0530

    adding userlists

--- components/lists/userList.tsx
+++ components/lists/userList.tsx
@@ -1,3 +1,70 @@
-const UserList = () => {};
+import { Colors } from '@constants/colors';
+import { Avatar, Stack, Typography } from '@mui/material';
+import { User } from '@prisma/client';
+import router from 'next/router';
+
+const UserList = ({ _each_user }: { _each_user: User }) => {
+  return (
+    <>
+      <div
+        style={{
+          maxWidth: 600,
+          backgroundColor: Colors.background,
+          border: `0.1pt inset ${Colors.standard_light_white}`,
+          borderRadius: '3px',
+          margin: '0px 10px',
+          cursor: 'pointer',
+        }}
+        onClick={() => router.push(`/user/${_each_user.username}`)}>
+        <Stack
+          direction='row'
+          spacing={1.5}
+          sx={{ marginLeft: 2, marginTop: 2, marginBottom: 2 }}>
+          <Avatar
+            sx={{ width: '45px', height: '45px' }}
+            variant='circular'
+            src={`${_each_user.image}`}
+          />
+          <Stack spacing={0.1} direction='column'>
+            <Typography
+              color={Colors.standard_white}
+              fontFamily={'Roboto'}
+              fontSize='1.1rem'
+              letterSpacing='0.025rem'
+              display={'flex'}
+              justifyContent='center'
+              alignItems={'center'}>
+              {_each_user.name}
+            </Typography>
+
+            <Typography
+              variant='body2'
+              fontWeight={400}
+              fontFamily={'Roboto Mono'}
+              fontSize={'0.65rem'}
+              letterSpacing='0.025rem'
+              display={'flex'}
+              justifyContent='center'
+              alignItems={'center'}
+              color={Colors.standard_light_white}>
+              {_each_user.username}
+            </Typography>
+          </Stack>
+        </Stack>
+        <Typography
+          variant='body2'
+          fontWeight={400}
+          fontFamily={'Roboto'}
+          fontSize={'1rem'}
+          letterSpacing='0.025rem'
+          alignItems={'left'}
+          margin='20px 20px 20px 20px'
+          color={Colors.standard_light_white}>
+          {_each_user.author_bio}
+        </Typography>
+      </div>
+    </>
+  );
+};
 
 export default UserList;
--- components/lists/userLists.tsx
+++ components/lists/userLists.tsx
@@ -1,5 +1,23 @@
 import UserList from '@components/lists/userList';
+import { Box } from '@mui/material';
+import { User } from '@prisma/client';
 
-const UserLists = () => {};
+const UserLists = ({ _userlist }: { _userlist: User[] }) => {
+  return (
+    <>
+      <Box
+        sx={{
+          maxWidth: 600,
+          margin: '20px auto',
+        }}>
+        {_userlist.map((_each_user) => {
+          return (
+            <UserList key={_each_user.id} _each_user={_each_user}></UserList>
+          );
+        })}
+      </Box>
+    </>
+  );
+};
 
 export default UserLists;
